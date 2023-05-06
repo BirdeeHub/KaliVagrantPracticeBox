7c73a8417c4af142e314c0b41d18a0e23db0e47c
BirdeeHub <robindbor@gmail.com>
2023-05-06 05:10:05 -0700

    aliases

--- .Provisioning/vagrantfile_scripts/7alias_script.sh
+++ .Provisioning/vagrantfile_scripts/7alias_script.sh
@@ -5,5 +5,5 @@ echo "alias gobuckets='gobuster vhost -w /usr/share/seclists/Discovery/DNS/subdo
 echo "alias srvsnake='sudo python3 -m http.server $1'" >> /home/vagrant/.zshrc
 echo "alias ovpn='sudo openvpn $1'" >> /home/vagrant/.zshrc
 echo "alias SCVnmap-p-='sudo nmap -vv -sSCV -p- -T4 $1'" >> /home/vagrant/.zshrc
-echo "alias gobusdir='gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u $1'" >> /home/vagrant/.zshrc
+echo "alias gobusdir='gobuster dir -v -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u $1'" >> /home/vagrant/.zshrc
 echo "alias THMssh='ssh -oHostKeyAlgorithms=+ssh-rsa $1'" >> /home/vagrant/.zshrc
