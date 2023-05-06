f632fb73c469d12a5144f96decd5074a8b6e1900
BirdeeHub <robindbor@gmail.com>
2023-05-06 04:54:31 -0700

    aliases

--- .Provisioning/vagrantfile_scripts/7alias_script.sh
+++ .Provisioning/vagrantfile_scripts/7alias_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo
 echo Adding Aliases to .zshrc file...
-echo "alias gobuckets='gobuster vhost -w ~/git_repos/SecLists/Discovery/DNS/subdomains-top1million-5000.txt --append-domain -u $1'" >> /home/vagrant/.zshrc
+echo "alias gobuckets='gobuster vhost -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt --append-domain -u $1'" >> /home/vagrant/.zshrc
 echo "alias srvsnake='sudo python3 -m http.server $1'" >> /home/vagrant/.zshrc
+echo "alias ovpn='sudo openvpn $1'" >> /home/vagrant/.zshrc
+echo "alias SCVnmap-p-='sudo nmap -vv -sSCV -p- -T4 $1'" >> /home/vagrant/.zshrc
+echo "alias gobusdir='gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u $1'" >> /home/vagrant/.zshrc
