8e9642264544a93e62a8375f679d48094daeaf43
BirdeeHub <robindbor@gmail.com>
2023-05-09 18:56:20 -0700

    function alias

--- .Provisioning/vagrantfile_scripts/7alias_script.sh
+++ .Provisioning/vagrantfile_scripts/7alias_script.sh
@@ -7,3 +7,5 @@ echo "alias ovpn='sudo openvpn $1'" >> /home/vagrant/.zshrc
 echo "alias SCVnmap-p-='sudo nmap -vv -sSCV -p- -T4 $1'" >> /home/vagrant/.zshrc
 echo "alias gobusdir='gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u $1'" >> /home/vagrant/.zshrc
 echo "alias THMssh='ssh -oHostKeyAlgorithms=+ssh-rsa $1'" >> /home/vagrant/.zshrc
+
+echo "function dirffufle() { echo \"\$#\"; local cnt=0; local xtraargs=(); for arg in \"\$@\"; do let \"cnt+=1\"; [[ \$cnt != 1 ]] &&  xtraargs+=(\"\$arg\"); [[ \$cnt == 1 ]] && local domain=\"\$arg\"; done; echo \"\$xtraargs\"; local full_command=\"sudo ffuf -c -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u http://\$domain/FUZZ \$xtraargs\"; bash -c \"\$full_command\"; }" >> /home/vagrant/.zshrc
