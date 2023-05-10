66d81e04bea7356cb5f58d14e3d9d2639626418e
BirdeeHub <robindbor@gmail.com>
2023-05-10 02:13:39 -0700

    couldnt escape the bugs

--- .Provisioning/vagrantfile_scripts/7alias_script.sh
+++ .Provisioning/vagrantfile_scripts/7alias_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo
 echo Adding Aliases to .zshrc file...
-echo "alias gobuckets='gobuster vhost -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -u $1'" >> /home/vagrant/.zshrc
-echo "alias srvsnake='sudo python3 -m http.server $1'" >> /home/vagrant/.zshrc
-echo "alias ovpn='sudo openvpn $1'" >> /home/vagrant/.zshrc
-echo "alias SCVnmap='sudo nmap -vv -sSCV -T4 $1'" >> /home/vagrant/.zshrc
-echo "alias SCVnmap-p-='sudo nmap -vv -sSCV -p- -T4 $1'" >> /home/vagrant/.zshrc
-echo "alias gobusdir='gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u $1'" >> /home/vagrant/.zshrc
-echo "alias THMssh='ssh -oHostKeyAlgorithms=+ssh-rsa $1'" >> /home/vagrant/.zshrc
+echo "alias gobuckets='gobuster vhost -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -u \$1'" >> /home/vagrant/.zshrc
+echo "alias srvsnake='sudo python3 -m http.server \$1'" >> /home/vagrant/.zshrc
+echo "alias ovpn='sudo openvpn \$1'" >> /home/vagrant/.zshrc
+echo "alias SCVnmap='sudo nmap -vv -sSCV -T4 \$1'" >> /home/vagrant/.zshrc
+echo "alias SCVnmap-p-='sudo nmap -vv -sSCV -p- -T4 \$1'" >> /home/vagrant/.zshrc
+echo "alias gobusdir='gobuster dir -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u \$1'" >> /home/vagrant/.zshrc
+echo "alias THMssh='ssh -oHostKeyAlgorithms=+ssh-rsa \$1'" >> /home/vagrant/.zshrc
 
 echo "function dirffufle() {local cnt=0; local xtraargs=(); for arg in \"\$@\"; do let \"cnt+=1\"; [[ \$cnt != 1 ]] &&  xtraargs+=(\"\$arg\"); [[ \$cnt == 1 ]] && local url=\"\$arg\"; done; local full_command=\"sudo ffuf -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt -u \$url/FUZZ \$xtraargs\"; echo \"\$full_command\"; bash -c \"\$full_command\"; }" >> /home/vagrant/.zshrc
 
