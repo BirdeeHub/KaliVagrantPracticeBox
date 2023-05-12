6a26427f8f4d2f6dc6f794a427d2d617dd7f082d
BirdeeHub <robindbor@gmail.com>
2023-05-12 15:51:22 -0700

    vnc stuff

--- .Provisioning/vnc/startnovnc.sh
+++ .Provisioning/vnc/startnovnc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tigervncserver -xstartup /usr/bin/startxfce4 -localhost  -rfbport 5900
 setsid /usr/share/novnc/utils/novnc_proxy --listen 8081 --vnc localhost:5900 >/dev/null 2>&1 < /dev/null &
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /vagrant/notes/DesktopBackgrounds/china-street-1920×1080.jpg
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /vagrant/.Provisioning/DesktopBackgrounds/china-street-1920×1080.jpg
